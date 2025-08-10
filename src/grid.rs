58adeec90452d3640b59ab80cddee670b7259fd2
Rabbival <87331993+Rabbival@users.noreply.github.com>
2025-08-10 14:00:34 +0300

    Add Transform as a required component to Grid<N>
    When spawning it without a transform, since the debug grid has a required transform, I'm getting:
    ``` warning[B0004]: Entity 261v2 with the GlobalTransform component has a parent without GlobalTransform.
    This will cause inconsistent behaviors! See: https://bevyengine.org/learn/errors/b0004
    ```

--- src/grid.rs
+++ src/grid.rs
@@ -342,7 +342,7 @@ impl Default for GridInternalSettings {
 ///    commands.spawn(grid);
 /// }
 /// ```
-#[derive(Component)]
+#[derive(Component, Transform)]
 pub struct Grid<N: Neighborhood> {
     pub(crate) neighborhood: N,
 
