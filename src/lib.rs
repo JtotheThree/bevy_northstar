d649d779dc4a2e4df997275683e8d648ad3fb313
JToTheThree <johnmullins@gmail.com>
2025-11-25 18:50:04 -0600

    Fix prelude

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@ pub mod prelude {
     pub use crate::MovementCost;
     pub use crate::NavRegion;
     pub use crate::SearchLimits;
-    pub use crate::{CardinalGrid, CardinalGrid3d, OrdinalGrid, OrdinalGrid3d};
+    pub use crate::{CardinalGrid, CardinalGrid3d, OrdinalGrid, OrdinalGrid3d, CardinalIsoGrid};
 }
 
 /// Alias for movement cost type.
